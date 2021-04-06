3837eae768c4932e714aa81138dbb06814976918
negrut-alin <negrutmarius18@stud.ase.ro>
2021-04-06 20:45:54 +0300

    Sample Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,50 @@
-public class Main {
+
+    // Design Pattern (Factory Method)
+
+    enum LOGISTICSEnum{
+        ROAD,
+        SEA
+    }
+
+    interface Logistics{
+        void transport();
+    }
+
+    class Road implements Logistics{
+
+        @Override
+        public void transport() {
+            System.out.println("Transport by Truck !");
+        }
+    }
+
+    class Sea implements  Logistics{
+
+        @Override
+        public void transport() {
+            System.out.println("Transport by Boat !");
+        }
+    }
+
+
+    class LogisticsFactory{
+        Logistics getLogistics(LOGISTICSEnum VAR){
+              if(VAR == LOGISTICSEnum.ROAD) {
+                  return new Road();
+              }
+
+             else{
+              return new Sea();
+             }
+        }
+    }
+
+public class Main{
     public static void main(String[] Args){
         System.out.println("Hello World !");
+        Logistics Var;
+        LogisticsFactory logisticsFactory = new LogisticsFactory();
+           Var =  logisticsFactory.getLogistics(LOGISTICSEnum.SEA);
+            Var.transport();
     }
 }
